3803d8aea1953b0b29e3dc0c17f8e65df0ed27c1
Cypher14x <alexcoro14x@gmail.com>
2025-07-04 10:05:30 -0500

    Aprendiendo promise.all

--- src/index.js
+++ src/index.js
@@ -22,10 +22,38 @@ const heroeId2 = 'spider';
 
 // });
 
-buscarHeroePromesa(heroeId).then(heroe => {
-    console.log(`Enviando a ${heroe.nombre} a la mision`);
+// Usando promesas pero sale lo mismo que callbacks hell
+// buscarHeroePromesa(heroeId).then(heroe => {
+//     // console.log(`Enviando a ${heroe.nombre} a la mision`);
+//     buscarHeroePromesa(heroeId2).then(heroe2 => {
+//         console.log(`Enviando a ${heroe.nombre} y ${heroe2.nombre} a la mision`);
+//     });
+// });
+
+// para no tener ese problema se hace asi 
+
+Promise.all([buscarHeroePromesa(heroeId),buscarHeroePromesa(heroeId2)])
+    .then(([heroe1, heroe2]) => {
+    
+    console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision`);
 
 });
+
+
+// buscarHeroePromesa(heroeId)
+//     .then(heroe1 => {
+//         return buscarHeroePromesa(heroeId2).then(heroe2 => {
+//             return {heroe1, heroe2};
+//         });
+//     })
+//     .then(({heroe1, heroe2}) => {
+//         console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision`);
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });
+                       
+
 
 
 console.log('fin de la app');
